b58265609448b5a1f7ce8866b856ab56a0b1dae7
KAMALAKAR810 <kamalakarmustari3@gmail.com>
2026-04-26 05:45:34 +0530

    mail11

--- MATKAAPP/axes_utils.py
+++ MATKAAPP/axes_utils.py
@@ -1,8 +1,26 @@
-def axes_whitelist(request, username, ip_address, **kwargs):
+def axes_whitelist(request, credentials=None, *args, **kwargs):
     """
     Exempt staff/superusers from django-axes lockouts.
     This prevents admins from getting locked while still protecting normal users.
     """
+
+    # django-axes has changed the whitelist callable signature across versions:
+    # - older: (request, username, ip_address, **kwargs)
+    # - newer: (request, credentials)
+    #
+    # To stay compatible, accept both and extract the username if present.
+    username = None
+    if isinstance(credentials, str):
+        # Called as (request, username, ip_address, ...)
+        username = credentials
+    elif isinstance(credentials, dict):
+        # Called as (request, credentials)
+        username = credentials.get("username") or credentials.get("email") or credentials.get("user")
+
+    # Some integrations might pass username as a kwarg.
+    if not username:
+        username = kwargs.get("username") or kwargs.get("user") or kwargs.get("email")
+
     if not username:
         return False
 
